7518668af3790108946a1e1bc23042686e1cd0b2
alberto.mila <alberto.mila@king.com>
2019-07-12 16:46:32 +0200

    first push

---
+++ Assets/MainManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        LevelManager.CreateInstance();
+        LevelManager go = LevelManager.GetInstance();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/MonoSingleton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance = null;
+
+    public static void CreateInstance()
+    {
+        Debug.Assert(_instance == null);
+        _instance = UnityEngine.Object.FindObjectOfType<T>();
+        Debug.Assert(_instance != null, "Instance " + typeof(T).ToString() + " doesn't exist");
+    }
+
+    public static T GetInstance()
+    {
+        Debug.Assert(_instance != null, "Instance doesn't exist");
+        return _instance;
+    }
+
+    public static bool IsValid()
+    {
+        return _instance != null;
+    }
+}
